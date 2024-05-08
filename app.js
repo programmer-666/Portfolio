196e3cb19d797c405982d93b5cf36b22dc040ec1
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-05-08 22:57:25 +0300

    update

--- app.js
+++ app.js
@@ -4,10 +4,18 @@ import morgan from "morgan";
 const app = express();
 
 app.use(express.static("public"));
-app.use(morgan("dev"));
+app.use(morgan("combined"));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
+});
+
+app.get("/blog", (req, res) => {
+    res.render("blog.ejs");
+});
+
+app.get("/projeler", (req, res) => {
+    res.render("projects.ejs");
 });
 
 app.listen("80");
