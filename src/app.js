8cd37273c9895ad809b4a5ade72bf281da85c359
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-05-12 23:00:58 +0300

    update

---
+++ src/app.js
@@ -0,0 +1,40 @@
+import express from "express";
+import compression from "compression";
+import morgan from "morgan";
+
+const app = express();
+const mainRoute = express.Router();
+
+app.use(express.static("../public"));
+app.use(compression());
+app.use(morgan("combined"));
+
+app.use("/", mainRoute);
+
+
+mainRoute.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+mainRoute.get("/blog", (req, res) => {
+    res.render("blogs.ejs");
+});
+
+mainRoute.get("/project-example", (req, res) => {
+    res.render("project.ejs");
+});
+
+mainRoute.get("/cv", (req, res) => {
+    res.render("cv.ejs");
+});
+
+mainRoute.get("/blog-example", (req, res) => {
+    res.render("blog.ejs");
+});
+
+mainRoute.get("/projeler", (req, res) => {
+    res.render("projects.ejs");
+});
+
+
+app.listen(process.env.PORT || 80);
