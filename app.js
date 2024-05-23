ee63df843e90d2573a6f69a275fd5ca6cff6b951
programmer-666 <34501436+programmer-666@users.noreply.github.com>
2024-05-23 12:36:29 +0300

    files update

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import morgan from "morgan";
-import express from "express";
 import compression from "compression";
-import { mainRouter } from "./routes/mainRoute.mjs";
+import express from "express";
+import morgan from "morgan";
 import path from "path";
+import { mainRouter } from "./routes/mainRoute.mjs";
 
 const app = express();
 
